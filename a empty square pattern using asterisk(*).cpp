95f01076f1c9da532bff713580e3b4996ceb83d9
Faique <60546073+Faiquekhan15@users.noreply.github.com>
2021-10-02 13:53:08 +0500

    Create a empty square pattern using asterisk(*).cpp
    The output of the code is like this.
    
    Enter size:
    5
    ****
    *  *
    *  *
    ****

---
+++ a empty square pattern using asterisk(*).cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+int main () 
+{
+  int s = 1;
+  int x = 0;
+  {
+    int y = 0;
+    cout << "Enter size:" << "\n";
+    cin >> x;
+    x = x / 2;
+    y = x;
+    while (x >= 1)
+    {
+      int i = 1;
+      while (i <= x)
+      {
+        cout << "*";
+        i++;
+      }
+      int j = s;
+      while (j > 1)
+      {
+        cout << "  ";
+        j--;
+      }
+      int k = x;
+      while (k >= 1)
+      {
+        cout << "*";
+        k--;
+      }
+      x--;
+      cout << endl;
+      s++;
+    }
+    int space = y;
+    int m = 1;
+    while (m <= y)
+    {
+      int i = 1;
+      while (i <= m)
+      {
+        cout << "*";
+        i++;
+      }
+      int j = space;
+      while (j > 1)
+      {
+        cout << "  ";
+        j--;
+      }
+      int f = m;
+      while (f >= 1)
+      {
+        cout << "*";
+        f--;
+      }
+      m++;
+      cout << endl;
+      space--;
+    }
+    return 0;
+  }
+}
